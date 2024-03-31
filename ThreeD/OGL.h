820d1436cb15b1e2425fa99ab9703f6369688605
Zombieschannel <54750550+Zombieschannel@users.noreply.github.com>
2024-03-31 13:05:42 +0200

    latest

--- ThreeD/OGL.h
+++ ThreeD/OGL.h
@@ -1,8 +1,6 @@
 #pragma once
 #ifdef __ANDROID__
 
-#define GLES31
-
 #ifdef GLES20
 #include <GLES2/gl2.h>
 #endif
@@ -21,7 +19,6 @@
 
 #else
 
-//#include <GL/glew.h>
 #include <glad/glad.h>
 
 #endif
